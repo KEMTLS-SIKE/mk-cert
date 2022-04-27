680bbfb72fb069d96749b686b2773dca3e7e6cd0
Guilhem Niot <guilhem@gniot.fr>
2022-04-27 10:50:53 +0200

    Update algorithms

--- algorithms.py
+++ algorithms.py
@@ -31,11 +31,11 @@ signs = [
 ]
 
 kems = [
-    ("kyber512", "Kyber512", False),
-    ("kyber768", "Kyber768", False),
-    ("kyber1024", "Kyber1024", False),
+    ("kyber512", "Kyber512"),
+    ("kyber768", "Kyber768"),
+    ("kyber1024", "Kyber1024"),
     *[
-        (f"classicmceliece{size}", f"ClassicMcEliece{size}", False)
+        (f"classicmceliece{size}", f"ClassicMcEliece{size}")
         for size in (
             "348864",
             "348864f",
@@ -49,32 +49,32 @@ kems = [
             "8192128f",
         )
     ],
-    ("lightsaber", "Lightsaber", False),
-    ("saber", "Saber", False),
-    ("firesaber", "Firesaber", False),
-    ("ntruhps2048509", "NtruHps2048509", False),
-    ("ntruhps2048677", "NtruHps2048677", False),
-    ("ntruhps4096821", "NtruHps4096821", False),
-    ("ntruhrss701", "NtruHrss701", False),
-    ("ntruprimentrulpr653", "NtruPrimeNtrulpr653", False),
-    ("ntruprimentrulpr761", "NtruPrimeNtrulpr761", False),
-    ("ntruprimentrulpr857", "NtruPrimeNtrulpr857", False),
-    ("ntruprimesntrup653",  "NtruPrimeSntrup653", False),
-    ("ntruprimesntrup761",  "NtruPrimeSntrup761", False),
-    ("ntruprimesntrup857",  "NtruPrimeSntrup857", False),
+    ("lightsaber", "Lightsaber"),
+    ("saber", "Saber"),
+    ("firesaber", "Firesaber"),
+    ("ntruhps2048509", "NtruHps2048509"),
+    ("ntruhps2048677", "NtruHps2048677"),
+    ("ntruhps4096821", "NtruHps4096821"),
+    ("ntruhrss701", "NtruHrss701"),
+    ("ntruprimentrulpr653", "NtruPrimeNtrulpr653"),
+    ("ntruprimentrulpr761", "NtruPrimeNtrulpr761"),
+    ("ntruprimentrulpr857", "NtruPrimeNtrulpr857"),
+    ("ntruprimesntrup653",  "NtruPrimeSntrup653"),
+    ("ntruprimesntrup761",  "NtruPrimeSntrup761"),
+    ("ntruprimesntrup857",  "NtruPrimeSntrup857"),
     *[
-        (f"frodokem{size}{alg}", f"FrodoKem{size.title()}{alg.title()}", False)
+        (f"frodokem{size}{alg}", f"FrodoKem{size.title()}{alg.title()}")
         for size in ("640", "976", "1344")
         for alg in ("aes", "shake")
     ],
     *[
-        (f"sikep{size}{compressed}{'async' if asynchronous else ''}", f"SikeP{size}{compressed.title()}", asynchronous)
+        (f"sikep{size}{compressed}", f"SikeP{size}{compressed.title()}")
         for size in ("434", "503", "610", "751")
-        for (compressed, asynchronous) in (("", False), ("compressed", False), ("compressed", True))
+        for compressed in ("", "compressed")
     ],
-    ("bikel1", "BikeL1", False),
-    ("bikel3", "BikeL3", False),
-    *[(f"hqc{size}", f"Hqc{size}", False) for size in ["128", "192", "256"]],
+    ("bikel1", "BikeL1"),
+    ("bikel3", "BikeL3"),
+    *[(f"hqc{size}", f"Hqc{size}") for size in ["128", "192", "256"]],
 ]
 
 
